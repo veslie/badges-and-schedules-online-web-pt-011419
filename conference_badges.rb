94c0fdcd5667f73b0fe0ea9e71758a8c2e6b2a59
veslie losius <veslielosius@yahoo.com>
2019-02-07 15:32:06 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+batch_badge(name)
+name.each do |name|
+  puts "Hello, my name is #{name}!"
+
+assign_rooms(attendees)
+count = 0 
+while count <= attendees.length 6
+puts "hello, #{attendees}! you'll be assign to room #{count}!"
+count +=1 
+
+ end
+end
